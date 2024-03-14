a2d75dbdce8cbec1eaab5d53789f3562fba8a459
Nenad <nesaeco@gmail.com>
2024-03-14 17:17:31 +0100

    Hardcode path for mongodb connection

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -8,7 +8,7 @@ export const connectedToDb = async () => {
       console.log('Use existing connection!')
       return
     }
-    const connection = await mongoose.connect(process.env.MONGO_DB)
+    const connection = await mongoose.connect('mongodb+srv://nesaeco:WJrXlGn8OjItQotq@cluster0.xc9wkfb.mongodb.net/menr')
     db.isConnected = connection.connections[0].readyState
     console.log('Connected to db...')
   } catch (error) {
