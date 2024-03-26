cfb675f7d3c33591c9111d519e0157b8edb31694
Nenad <nesaeco@gmail.com>
2024-03-27 00:32:33 +0100

    Terminate usage of existing connection

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -4,10 +4,10 @@ let db = {}
 
 export const connectedToDb = async () => {
   try {
-    if(db.isConnected) {
-      console.log('Use existing connection!')
-      return
-    }
+    // if(db.isConnected) {
+    //   console.log('Use existing connection!')
+    //   return
+    // }
     const connection = await mongoose.connect('mongodb+srv://nesaeco:WJrXlGn8OjItQotq@cluster0.xc9wkfb.mongodb.net/menr')
     db.isConnected = connection.connections[0].readyState
     console.log('Connected to db...')
