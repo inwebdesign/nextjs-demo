0dfa998e2c4331cdec9bc381b15f43819157f9ef
Nenad <nesaeco@gmail.com>
2024-03-27 00:12:11 +0100

    Refactor fetch with axios library

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,22 +1,39 @@
+'use client'
 import Card from '@/components/card/Card'
+import axios from 'axios';
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react';
 // import { getSinglePost } from '@/app/lib/data'
 
-const getSinglePost = async (slug) => {
-  try {
-    const url = 'http://127.0.0.1:3000'
-    const post = await fetch(`${process.env.NEXT_PUBLIC_API_URL || url}/api/blog/${slug}`)
-    if(!post) throw new Error("No post found")
-    return post.json()
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const SingleBlog = async ({params}) => {
-  const {slug} = params
-  const singlePost = await getSinglePost(slug)
-  const {title, desc, image} = singlePost
-  return  <>{singlePost && (<Card title={title} desc={desc} image={image}/>)}</>  
+// const getSinglePost = async (slug) => {
+  // try {
+    //   const url = 'http://127.0.0.1:3000'
+    //   const post = await fetch(`${process.env.NEXT_PUBLIC_API_URL || url}/api/blog/${slug}`)
+    //   if(!post) throw new Error("No post found")
+    //   return post.json()
+    // } catch (error) {
+      //   console.log(error)
+      // }
+    // }
+    
+    const SingleBlog = ({params}) => {
+      const {slug} = params
+      const [post, setPost] = useState(null)
+      const url = process.env.NEXT_PUBLIC_API_URL
+      useEffect(() => {
+        axios.get(`${url}/api/blog/${slug}`)
+        .then(function (response) { 
+          setPost(response.data);
+          return post
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }, []);
+  // const {slug} = params
+  // const singlePost = await getSinglePost(slug)
+  // const {title, desc, image} = singlePost
+  return  <>{post && (<Card title={post?.title} desc={post?.desc} image={post?.image}/>)}</>  
   
 }
 
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,22 +1,48 @@
+'use client'
 import Card from '@/components/card/Card'
 import styles from './Blog.module.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const getPosts = async () => {
-  try {
-    const url = 'http://127.0.0.1:3000'
-    const posts = await fetch(`${process.env.NEXT_PUBLIC_API_URL || url}/api/blog`)
-    if(!posts) throw new Error('No posts found')
-    return posts.json()
-  } catch (error) {
-    console.log(error)
-  }
-}
+// const getPosts = async () => {
+//   try {
+//     const url = 'http://127.0.0.1:3000'
+//     const posts = await fetch(`${process.env.NEXT_PUBLIC_API_URL || url}/api/blog`)
+//     if(!posts) throw new Error('No posts found')
+//     return posts.json()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// const getPosts = async () => {
+//   const url = 'http://127.0.0.1:3000'
+//   await axios.get(`${url}/api/blog`).then(response => {
+//     // if(!response) {
+//     //   throw new Error('No posts found')
+//     // }
+//     console.log(response.data)
+//     return response.data
+//   }).catch((err) => console.error(err)) 
+// }
 
-const Blog = async () => {
 
+
+const Blog = () => {
+  const [posts, setPosts] = useState([])
+  const url = process.env.NEXT_PUBLIC_API_URL
+  useEffect(() => {
+    axios.get(`${url}/api/blog`)
+      .then(function (response) { 
+        setPosts(response.data);
+        return posts
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 // const posts = await getPosts()
-const posts = await getPosts()
-
+// const posts = await getPosts()
+// console.log(posts)
   return (
     <div className={styles.blog}>
       {posts?.map(({title, desc, image, slug}) => <Card title={title} desc={desc} image={image} slug={slug} key={title}/>)}
